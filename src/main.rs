7fdd10c9806ebb3a00edfaab4ef90f4bcf90f1ae
Bloom <workmailsofnityaprakash@gmail.com>
2025-12-21 02:22:10 +0530

    Initial Rust streaming chat client

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::io::{self, Write};
+
+fn main() {
+    print!(">>> ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    println!("You said: {}", input);
+}
